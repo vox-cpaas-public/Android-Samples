b25020fdb19080b344ecdd36a3dbe0aaa4b53407
voxvalley <45165034+vox-cpaas-public@users.noreply.github.com>
2020-03-16 06:22:35 +0530

    updated to latest sdk

--- ClickToCall/app/src/main/java/com/ca/clicktocall/ClickToCallConstants.java
+++ ClickToCall/app/src/main/java/com/ca/clicktocall/ClickToCallConstants.java
@@ -8,10 +8,10 @@ public class ClickToCallConstants {
     public static String projectname = "ClickToCall";
     public static String projectid = "pid_39684a6d_5103_4254_9775_1b923b9b98d5";
 
-    public static String loginid = "+919492084600";
+    public static String loginid = "+9194920xxxxx";
     public static String password = "test";
 
-    public static String destinationid = "+917794092658";
+    public static String destinationid = "+9177940xxxxx";
 
     public static boolean iSHoldCalled = false;
     public static boolean isintentionalhold = false;
